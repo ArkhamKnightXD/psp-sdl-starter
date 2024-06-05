91f169b269ccc8c3164e4421ddebcbc2d9bcf381
Knight <karvin_jimenez@hotmail.com>
2024-06-04 22:34:01 -0400

    improvements to the code.

--- src/main.cpp
+++ src/main.cpp
@@ -32,7 +32,6 @@ int setupCallbacks(void) {
     return thid;
 }
 
-// Screen dimension constants
 enum {
   SCREEN_WIDTH  = 480,
   SCREEN_HEIGHT = 272
@@ -40,17 +39,14 @@ enum {
 
 SDL_Window *window = NULL;
 SDL_Renderer *renderer = NULL;
-SDL_GameController* controller = NULL;
-SDL_Texture* sprite;
+SDL_GameController *controller = NULL;
+SDL_Texture *sprite;
+SDL_Rect spriteBounds = {SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 64, 64};
 
-bool shouldRenderSprite = false;
-
-SDL_Rect rectangle = {SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 24, 24};
+bool shouldRenderSprite = true;
 
 const int SPEED = 200; 
-const int FRAME_RATE = 60; 
 
-// Exit the game and clean up
 void quitGame() {
 
     SDL_GameControllerClose(controller);
@@ -59,7 +55,6 @@ void quitGame() {
     SDL_Quit();
 }
 
-// Function to handle events
 void handleEvents() {
 
     SDL_Event event;
@@ -74,63 +69,46 @@ void handleEvents() {
     }
 }
 
-void capFrameRate(Uint32 frameStartTime) {
-
-    Uint32 frameTime = SDL_GetTicks() - frameStartTime;
-    
-    if (frameTime < 1000 / FRAME_RATE) {
-        SDL_Delay(1000 / FRAME_RATE - frameTime);
-    }
-}
-
-SDL_Texture* LoadSprite(const char* file, SDL_Renderer* renderer) {
+SDL_Texture* loadSprite(const char* file, SDL_Renderer* renderer) {
 
     SDL_Texture* texture = IMG_LoadTexture(renderer, file);
-    if (texture == nullptr) {
+    // if (texture == nullptr) {
 
-        pspDebugScreenPrintf("Failed to create texture: %s\n", SDL_GetError());
-        sceKernelDelayThread(3 * 1000 * 1000);
-        return nullptr;
-    }
+    //     pspDebugScreenPrintf("Failed to create texture: %s\n", SDL_GetError());
+    //     sceKernelDelayThread(2 * 1000 * 1000);
+    //     return nullptr;
+    // }
 
-    pspDebugScreenPrintf("Loaded image: %s\n", file);
-    sceKernelDelayThread(3 * 1000 * 1000);
+    // pspDebugScreenPrintf("Loaded image: %s\n", file);
+    // sceKernelDelayThread(2 * 1000 * 1000);
 
     return texture;
 }
 
-void RenderSprite(SDL_Texture* sprite, SDL_Renderer* renderer, int x, int y) {
+void renderSprite(SDL_Texture* sprite, SDL_Renderer* renderer, SDL_Rect spriteBounds) {
 
-    SDL_Rect dest;
-    dest.x = x;
-    dest.y = y;
-
-    SDL_QueryTexture(sprite, NULL, NULL, &dest.w, &dest.h);
-    SDL_RenderCopy(renderer, sprite, NULL, &dest);
+    SDL_QueryTexture(sprite, NULL, NULL, &spriteBounds.w, &spriteBounds.h);
+    SDL_RenderCopy(renderer, sprite, NULL, &spriteBounds);
 }
 
 void update(float deltaTime) {
 
     SDL_GameControllerUpdate();
 
-    if (SDL_GameControllerGetButton(controller, SDL_CONTROLLER_BUTTON_A)) {
-        shouldRenderSprite = !shouldRenderSprite;
+    if (SDL_GameControllerGetButton(controller, SDL_CONTROLLER_BUTTON_DPAD_UP) && spriteBounds.y > 0) {
+        spriteBounds.y -= SPEED * deltaTime;
     }
 
-    if (SDL_GameControllerGetButton(controller, SDL_CONTROLLER_BUTTON_DPAD_UP) && rectangle.y > 0) {
-        rectangle.y -= SPEED * deltaTime;
+    else if (SDL_GameControllerGetButton(controller, SDL_CONTROLLER_BUTTON_DPAD_DOWN) && spriteBounds.y < SCREEN_HEIGHT - spriteBounds.h) {
+        spriteBounds.y += SPEED * deltaTime;
     }
 
-    else if (SDL_GameControllerGetButton(controller, SDL_CONTROLLER_BUTTON_DPAD_DOWN) && rectangle.y < SCREEN_HEIGHT - rectangle.h) {
-        rectangle.y += SPEED * deltaTime;
+    else if (SDL_GameControllerGetButton(controller, SDL_CONTROLLER_BUTTON_DPAD_LEFT) && spriteBounds.x > 0) {
+        spriteBounds.x -= SPEED * deltaTime;
     }
 
-    else if (SDL_GameControllerGetButton(controller, SDL_CONTROLLER_BUTTON_DPAD_LEFT) && rectangle.x > 0) {
-        rectangle.x -= SPEED * deltaTime;
-    }
-
-    else if (SDL_GameControllerGetButton(controller, SDL_CONTROLLER_BUTTON_DPAD_RIGHT) && rectangle.x < SCREEN_WIDTH - rectangle.w) {
-        rectangle.x += SPEED * deltaTime;
+    else if (SDL_GameControllerGetButton(controller, SDL_CONTROLLER_BUTTON_DPAD_RIGHT) && spriteBounds.x < SCREEN_WIDTH - spriteBounds.w) {
+        spriteBounds.x += SPEED * deltaTime;
     }
 }
 
@@ -139,13 +117,9 @@ void render() {
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
 
+    renderSprite(sprite, renderer, spriteBounds);
+
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-
-    if(shouldRenderSprite) {
-        RenderSprite(sprite, renderer, 100, 100);
-    }
-
-    SDL_RenderFillRect(renderer, &rectangle);
 
     SDL_RenderPresent(renderer);  
 }
@@ -153,14 +127,13 @@ void render() {
 int main() {
 
     setupCallbacks();
-    SDL_SetMainReady();
     pspDebugScreenInit();
 
     if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_GAMECONTROLLER) < 0) {
         return -1;
     }
 
-    if ((window = SDL_CreateWindow("RedRectangle", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, 0)) == NULL) {
+    if ((window = SDL_CreateWindow("SDL-STARTER", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, 0)) == NULL) {
         return -1;
     }
 
@@ -177,12 +150,13 @@ int main() {
 
         controller = SDL_GameControllerOpen(0);
         if (controller == NULL) {
+
             pspDebugScreenPrintf("unable to open game controller");
             return -1;
         }
     }
 
-    sprite = LoadSprite("sprites/sprite.png", renderer);
+    sprite = loadSprite("sprites/sprite.png", renderer);
 
     Uint32 previousFrameTime = SDL_GetTicks();
     Uint32 currentFrameTime;
@@ -197,7 +171,6 @@ int main() {
         handleEvents();
         update(deltaTime);
         render();
-        // capFrameRate(currentFrameTime);
     }
 
     quitGame();
